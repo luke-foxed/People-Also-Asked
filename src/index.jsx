796406502fe617ed4ba204c039f6db7fb010246a
Luke Fox <luke.fox@distilled.ie>
2019-07-24 23:40:12 +0100

    Added semantic ui

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Favicon from 'react-favicon';
 import axios from 'axios';
+import { Header, Icon, Input, Container, Form, Button } from 'semantic-ui-react';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			search: 'Enter Search Query',
+			search: 'Search query',
 			loading: '',
 			response: []
 		};
@@ -36,23 +37,37 @@ class App extends React.Component {
 		this.fetchResults(this.refs.search.value);
 	}
 
-	render() {
+  handleMessage = e => { 
+    console.log('clicked')
+    console.log(e.target.value); }
 
-    const responseItems = this.state.response.map((res) =>
-    <li>{res}</li>
-  );
+	render() {
+		const responseItems = this.state.response.map((res) => <li>{res}</li>);
 
 		return (
-			<form onSubmit={this.startScraper}>
-				<Favicon url="https://raw.githubusercontent.com/aerobatic/react-starter/master/app/react.ico" />
-				<label>
-					{this.state.search}
-					<input type="text" name="name" ref="search" />
-				</label>
-				<input type="submit" value="Submit" />
-				<h1>{this.state.loading}</h1>
-        <ol>{responseItems}</ol>
-			</form>
+			<Container textAlign="center">
+				<br />
+				<Header color="blue" as="h2" icon textAlign="center">
+					<Icon inverted color="blue" name="google" circular />
+					<Header.Content>Google Scraper</Header.Content>
+				</Header>
+
+				<br />
+				<br />
+
+        <Input action={{ color: 'blue', icon: 'search' }} ref='search' onClick={this.handleMessage} />
+
+				<form onSubmit={this.startScraper}>
+					<Favicon url="https://raw.githubusercontent.com/aerobatic/react-starter/master/app/react.ico" />
+					<label>
+						{this.state.search}
+						<input type="text" name="name" ref="search" />
+					</label>
+					<input type="submit" value="Submit" />
+					<h1>{this.state.loading}</h1>
+					<ol>{responseItems}</ol>
+				</form>
+			</Container>
 		);
 	}
 }
