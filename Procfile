0dc218698e7e151ccd14b58da7499f8f11fe965d
Foxyf76 <lukefox5@gmail.com>
2019-07-27 11:27:09 +0100

    changed to waitress

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:app
+web: waitress-serve server:app
--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ chrome_options = Options()
 chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
 browser = webdriver.Chrome(executable_path=os.environ.get(
     'CHROMEDRIVER'), chrome_options=chrome_options)
 
