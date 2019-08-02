04bb46bc7bc9ea5869cb9620a36bce2f5ec6c5b9
Foxyf76 <lukefox5@gmail.com>
2019-08-02 22:50:04 +0100

    New header design

--- src/ResultsList.jsx
+++ src/ResultsList.jsx
@@ -1,99 +1,120 @@
-import React from 'react';
-import { Icon, Segment, Button, List, Popup, Message } from 'semantic-ui-react';
+import React from "react";
+import {
+  Icon,
+  Segment,
+  Button,
+  List,
+  Popup,
+  Message,
+  Label
+} from "semantic-ui-react";
 
 class ResultsList extends React.Component {
-	state = { showing: false };
+  state = { showing: false };
 
-	render() {
-		const { questions } = this.props;
-		const { showing } = this.state;
+  render() {
+    const { questions } = this.props;
+    const { showing } = this.state;
 
-		if (questions.length > 0) {
-			return (
-				<div>
-					{questions.map((question, index) => {
-						return (
-							<List.Item className="my_list_v1">
-								<List.Content>
-									<List.Header className="uppercase_header" as="h3">
-										{question.search}
-									</List.Header>
+    if (questions.length > 0) {
+      return (
+        <div>
+          {questions.map((question, index) => {
+            return (
+              <List.Item className='my_list_v1'>
+                <List.Content>
+                  <List.Header className='uppercase_header' as='h3'>
+                    {question.search}
+                  </List.Header>
 
-									<List.Description className="list_description">{question.more}</List.Description>
-									<br />
-									<br />
-									<Button.Group size="medium">
-										<Popup
-											inverted
-											wide="very"
-											content={question.article_header}
-											trigger={
-												<Button
-													icon="external"
-													content="Continue Reading"
-													as="a"
-													href={question.article_url}
-													target="_blank"
-												/>
-											}
-										/>
-										<Button.Or />
-										<Button onClick={() => this.setState({ showing: !showing })} color="blue">
-											<Icon name="dropdown" />
-											See Related
-										</Button>
-									</Button.Group>
-								</List.Content>
-								<List.List>
-									<br />
+                  <List.Description className='list_description'>
+                    {question.more}
+                  </List.Description>
+                  <br />
+                  <br />
+                  <Button.Group size='medium'>
+                    <Popup
+                      inverted
+                      wide='very'
+                      content={question.article_header}
+                      trigger={
+                        <Button
+                          icon='external'
+                          content='Continue Reading'
+                          as='a'
+                          href={question.article_url}
+                          target='_blank'
+                        />
+                      }
+                    />
+                    <Button.Or />
+                    <Button
+                      onClick={() => this.setState({ showing: !showing })}
+                      color='blue'
+                    >
+                      <Icon name='dropdown' />
+                      See Related
+                    </Button>
+                  </Button.Group>
+                </List.Content>
+                <List.List>
+                  <br />
 
-									{question.children.map((child, index) => {
-										return (
-											<Segment padded style={{ display: showing ? 'block' : 'none' }}>
-												<List.Item>
-													<List.Content>
-														<List.Header className="uppercase_header" as="h4">
-															{child.search}
-														</List.Header>
-														<List.Description className="list_description">
-															{child.more}
-														</List.Description>
-														<br />
+                  {question.children.map((child, index) => {
+                    return (
+                      <Segment
+                        padded
+                        style={{ display: showing ? "block" : "none" }}
+                      >
+                        <List.Item>
+                          <List.Content>
+                            <List.Header className='uppercase_header' as='h4'>
+                              {child.search}
+                            </List.Header>
+                            <List.Description className='list_description'>
+                              {child.more}
+                            </List.Description>
+                            <br />
 
-														<Popup
-															inverted
-															wide="very"
-															content={child.article_header}
-															trigger={
-																<Button
-																	icon="external"
-																	content="Continue Reading"
-																	as="a"
-																	href={child.article_url}
-																	target="_blank"
-																/>
-															}
-														/>
-													</List.Content>
-												</List.Item>
-											</Segment>
-										);
-									})}
-								</List.List>
-							</List.Item>
-						);
-					})}
-				</div>
-			);
-		}
+                            <Popup
+                              inverted
+                              wide='very'
+                              content={child.article_header}
+                              trigger={
+                                <Button
+                                  icon='external'
+                                  content='Continue Reading'
+                                  as='a'
+                                  href={child.article_url}
+                                  target='_blank'
+                                />
+                              }
+                            />
+                          </List.Content>
+                        </List.Item>
+                      </Segment>
+                    );
+                  })}
+                </List.List>
+              </List.Item>
+            );
+          })}
+          <br />
+          <br />
+          <Label attached='bottom' color='blue' size='small'>
+            SEO Scraper - Created by Luke Fox
+          </Label>
+        </div>
+      );
+    }
 
-		return (
-			<Message negative>
-				<Message.Header>No results found!</Message.Header>
-				<p>Try another search term</p>
-			</Message>
-		);
-	}
+    return (
+      <Message negative>
+        <Message.Header>No results found!</Message.Header>
+        <p>Try another search term</p>
+      </Message>
+    );
+  }
 }
 
 export default ResultsList;
--- src/index.jsx
+++ src/index.jsx
@@ -192,163 +192,175 @@
 // 	}
 // };
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./style.css";
 import {
-	Header,
-	Icon,
-	Input,
-	Container,
-	Segment,
-	Button,
-	List,
-	Grid,
-	Divider,
-	Label,
-	Dimmer,
-	Loader
-} from 'semantic-ui-react';
+  Header,
+  Icon,
+  Input,
+  Container,
+  Segment,
+  Button,
+  List,
+  Grid,
+  Divider,
+  Label,
+  Dimmer,
+  Loader,
+  Image
+} from "semantic-ui-react";
 
-import ResultsList from './ResultsList.jsx';
+import ResultsList from "./ResultsList.jsx";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchTerm: '',
-			response: [],
-			responseLoaded: false,
-			buttonDisabled: false,
-			loaderEnabled: false
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      response: [],
+      responseLoaded: false,
+      buttonDisabled: false,
+      loaderEnabled: false
+    };
 
-		this.fetchResults = this.fetchResults.bind(this);
-	}
+    this.fetchResults = this.fetchResults.bind(this);
+  }
 
-	fetchResults(arg) {
-		console.log(`fetching ${arg}...`);
-		axios.get('/scrape/' + arg).then((res) => {
-			console.log(res.data);
-			this.setState({
-				response: res.data,
-				buttonDisabled: false,
-				responseLoaded: true,
-				loaderEnabled: false
-			});
-		});
-	}
+  fetchResults(arg) {
+    console.log(`fetching ${arg}...`);
+    axios.get("/scrape/" + arg).then(res => {
+      console.log(res.data);
+      this.setState({
+        response: res.data,
+        buttonDisabled: false,
+        responseLoaded: true,
+        loaderEnabled: false
+      });
+    });
+  }
 
-	onButtonClick = () => {
-		this.setState({
-			searchTerm: this.state.value,
-			buttonDisabled: true,
-			loaderEnabled: true
-		});
-		this.fetchResults(this.state.value);
-	};
+  onButtonClick = () => {
+    this.setState({
+      searchTerm: this.state.value,
+      buttonDisabled: true,
+      loaderEnabled: true
+    });
+    this.fetchResults(this.state.value);
+  };
 
-	render() {
-		const { response } = this.state;
+  render() {
+    const { response } = this.state;
 
-		if (this.state.responseLoaded) {
-			return (
-				<Container textAlign="center">
-					<br />
-					<Header color="blue" as="h1" icon>
+    if (this.state.responseLoaded) {
+      return (
+        <div>
+          <Container textAlign='center' className='header_container'>
+            <br />
+            <Image
+              className='header_image'
+              src='https://i.ibb.co/q1fNRDs/logo-invertedv4.png'
+              size='large'
+            />
+          </Container>
+          <br />
+          <br />
+          <Container textAlign='center'>
+            <Input
+              size='large'
+              action={() => (
+                <Button
+                  disabled={this.state.buttonDisabled}
+                  color='blue'
+                  animated
+                  onClick={this.onButtonClick}
+                >
+                  <Button.Content visible>Search</Button.Content>
+                  <Button.Content hidden>
+                    <Icon name='search' />
+                  </Button.Content>
+                </Button>
+              )}
+              onChange={evt => this.setState({ value: evt.target.value })}
+            />
+
+            <br />
+            <br />
+
+            <Divider horizontal>
+              <Icon name='question circle outline' />
+              &nbsp; People Also Asked
+            </Divider>
+            <Grid padded>
+              <Header textAlign='left'>
+                <Icon name='search' />
+                <Header.Content className='uppercase_header'>
+                  You Searched: '{this.state.searchTerm}'
+                </Header.Content>
+              </Header>
+              <Segment compact stacked>
+                <Dimmer active={this.state.loaderEnabled} inverted>
+                  <Loader indeterminate>Scraping Searches...</Loader>
+                </Dimmer>
+                <List>
+                  <ResultsList questions={response.group1.questions} />
+                </List>
+              </Segment>
+            </Grid>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Container textAlign='center' className='header_container'>
+            <br />
+            <Image
+              className='header_image'
+              src='https://i.ibb.co/q1fNRDs/logo-invertedv4.png'
+              size='large'
+            />
+            {/* <Header color="blue" as="h1" icon>
 						<Icon inverted color="blue" name="google" circular />
 						<Header.Content>Google Scraper</Header.Content>
-					</Header>
+					</Header> */}
+          </Container>
+          <br />
+          <br />
+          <Container textAlign='center'>
+            <Input
+              size='large'
+              action={() => (
+                <Button
+                  disabled={this.state.buttonDisabled}
+                  color='blue'
+                  animated
+                  onClick={this.onButtonClick}
+                >
+                  <Button.Content visible>Search</Button.Content>
+                  <Button.Content hidden>
+                    <Icon name='search' />
+                  </Button.Content>
+                </Button>
+              )}
+              onChange={evt => this.setState({ value: evt.target.value })}
+            />
 
-					<br />
-					<br />
+            <Segment className='loading_segment'>
+              <Dimmer active={this.state.loaderEnabled} inverted>
+                <Loader indeterminate>Scraping Searches...</Loader>
+              </Dimmer>
+            </Segment>
 
-					<Input
-						action={() => (
-							<Button
-								disabled={this.state.buttonDisabled}
-								color="blue"
-								animated
-								onClick={this.onButtonClick}
-							>
-								<Button.Content visible>Search</Button.Content>
-								<Button.Content hidden>
-									<Icon name="search" />
-								</Button.Content>
-							</Button>
-						)}
-						onChange={(evt) => this.setState({ value: evt.target.value })}
-					/>
-
-					<br />
-					<br />
-
-					<Divider horizontal>
-						<Icon name="question circle outline" />
-						&nbsp; People Also Asked
-					</Divider>
-					<Grid padded>
-						<Header textAlign="left">
-							<Icon name="search" />
-							<Header.Content className="uppercase_header">
-								You Searched: '{this.state.searchTerm}'
-							</Header.Content>
-						</Header>
-						<Segment compact stacked>
-							<Dimmer active={this.state.loaderEnabled} inverted>
-								<Loader indeterminate>Scraping Searches...</Loader>
-							</Dimmer>
-							<List>
-								<ResultsList questions={response.group1.questions} />
-							</List>
-						</Segment>
-					</Grid>
-				</Container>
-			);
-		} else {
-			return (
-				<Container textAlign="center">
-					<br />
-					<Header color="blue" as="h1" icon>
-						<Icon inverted color="blue" name="google" circular />
-						<Header.Content>Google Scraper</Header.Content>
-					</Header>
-
-					<br />
-					<br />
-
-					<Input
-						action={() => (
-							<Button
-								disabled={this.state.buttonDisabled}
-								color="blue"
-								animated
-								onClick={this.onButtonClick}
-							>
-								<Button.Content visible>Search</Button.Content>
-								<Button.Content hidden>
-									<Icon name="search" />
-								</Button.Content>
-							</Button>
-						)}
-						onChange={(evt) => this.setState({ value: evt.target.value })}
-					/>
-
-					<Segment className="loading_segment">
-						<Dimmer active={this.state.loaderEnabled} inverted>
-							<Loader indeterminate>Scraping Searches...</Loader>
-						</Dimmer>
-					</Segment>
-
-					<Label attached="bottom right" color="blue" size="small">
-						For Educational Purposes Only
-					</Label>
-				</Container>
-			);
-		}
-	}
+            <Label attached='bottom right' color='blue' size='small'>
+              For Educational Purposes Only
+            </Label>
+          </Container>
+        </div>
+      );
+    }
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
