2a261f5ac825ceb26ba8d3aac3eae1d168ab43a4
Foxyf76 <lukefox5@gmail.com>
2019-08-17 18:03:27 +0100

    New logo, fixed url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  entry: ['./src/Index.jsx'],
+  entry: ["./src/Index.jsx"],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
 
       {
         test: /\.css/,
-        loaders: ['style-loader', 'css-loader']
+        loaders: ["style-loader", "css-loader"]
       },
 
       {
         test: /\.(png|jp(e*)g|svg)$/,
         use: [
           {
-            loader: 'url-loader'
+            loader: "url-loader"
           }
         ]
       }
     ]
   },
   output: {
-    path: __dirname + '/static',
-    filename: 'bundle.js'
+    path: __dirname + "/static",
+    filename: "bundle.js"
   }
 };
